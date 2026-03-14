ee63e969061275fa34e344557c3de8d13f7c82e4
amirmahdi1390 <amirmahdisultani65@gmail.com>
2026-03-14 22:39:29 +0430

    lazy importing and making route that mach to react router  feature got added

---
+++ generator.js
@@ -0,0 +1,22 @@
+export function generateRoutesModule(routes) {
+
+   
+    const imports = routes.map((route, index) => {
+      return `const Page${index} = lazy(() => import('${route.file}'))`
+    }).join('\n')
+  
+   
+    const routeItems = routes.map((route, index) => {
+      return `  { path: '${route.path}', element: <Page${index} /> }`
+    }).join(',\n')
+  
+    return `
+  import { lazy } from 'react'
+  
+  ${imports}
+  
+  export const routes = [
+  ${routeItems}
+  ]
+    `.trim()
+  }
